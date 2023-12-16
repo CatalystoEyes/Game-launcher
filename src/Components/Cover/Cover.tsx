8767e8d320525292b6d865d2cdf02bb4dfb18588
Artemy Catalysto <xmajioff@gmail.com>
2023-12-16 22:13:34 +0300

    "Configure commit"

--- src/Components/Cover/Cover.tsx
+++ src/Components/Cover/Cover.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren } from 'react'
 
 const Cover: FC<PropsWithChildren> = ({ children }) => {
     return (
--- src/Components/forms/CreateGame.tsx
+++ src/Components/forms/CreateGame.tsx
@@ -29,7 +29,7 @@ const CreateGame = () => {
         <label>
           <input className="input input-bordered max-w-xs" type="text" placeholder="Title" value={game.name} onChange={e => setGame({ ...game, name: e.target.value })} />
           <input className="input input-bordered max-w-xs mx-1" type="text" placeholder="Image" value={game.image} onChange={e => setGame({ ...game, image: e.target.value })} />
-          <input className="input input-bordered max-w-xs" type="text" placeholder="Description" value={game.description} onChange={e => setGame({ ...game, description: e.target.value })} />
+          <input className="input input-bordered max-w-xs mx-1" type="text" placeholder="Description" value={game.description} onChange={e => setGame({ ...game, description: e.target.value })} />
           <input className="input input-bordered max-w-xs" type="text" placeholder="Price" value={game.price} onChange={e => setGame({ ...game, price: e.target.value })} />
           <button type="submit" className="btn btn-outline mx-1">Create</button>
         </label>
